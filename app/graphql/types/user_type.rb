c0fb28a6897184caae2f855708febaf3b71ddc72
Nikhil Pathak <nikhilpathak926@gmail.com>
2020-02-24 11:23:09 +0530

    Added attributes in user and book object types

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,4 +1,18 @@
 module Types
   class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: true
+    field :email, String, null: true
+    field :books, [Types::BookType], null: true
+    field :books_count, Integer, null: true
+    field :first_book, Types::BookType, null: true
+
+    def books_count
+      object.books.count
+    end
+
+    def first_book
+      object.books.first
+    end
   end
 end
