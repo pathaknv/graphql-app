05012acac7d6f4e949b45c6d92a6ba7f90cad603
Nikhil Pathak <nikhil.pathak@velotio.com>
2020-03-03 12:28:30 +0530

    Seed file for dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+10.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+  rand(1..10).times do
+    user.books.create(title: Faker::Book.title)
+  end
+end
