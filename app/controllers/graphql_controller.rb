2f2f6a9c7ee3a34a530c9ebb7d7ec178db22781f
Nikhil Pathak <nikhil.pathak@velotio.com>
2020-03-03 12:23:04 +0530

    Renamed the application

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -12,7 +12,7 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
-    result = GraphqlSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = GraphqlAppSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue => e
     raise e unless Rails.env.development?
--- app/graphql/graphql_app_schema.rb
+++ app/graphql/graphql_app_schema.rb
@@ -1,4 +1,4 @@
-class GraphqlSchema < GraphQL::Schema
+class GraphqlAppSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
