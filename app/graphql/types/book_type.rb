bbb261a25cac895357d19783e6ba91f220469daa
Nikhil Pathak <nikhil.pathak@velotio.com>
2020-03-03 14:48:44 +0530

    GraphQL json scalar field

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -6,11 +6,16 @@ module Types
     field :title, String, null: false
     field :user_id, ID, null: false
     field :user_name, String, null: false
+    field :user_details, GraphQL::Types::JSON, null: false
 
     def user_name
       BatchLoader::GraphQL.for(object.user_id).batch do |user_ids, loader|
         User.find(user_ids).each { |user| loader.call(user.id) { user.name } }
       end
+    end
+
+    def user_details
+      { user_id: object.user.id, user_name: object.user.name }
     end
   end
 end
